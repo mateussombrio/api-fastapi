f35fd61cd47035e508a56bf4b4516bfb183014ff
Mateus Sombrio <mateusombrio@gmail.com>
2025-12-11 17:07:46 -0300

    Aula 3

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
-from fast_zero.schemas import Message
+from http import HTTPStatus
+
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
 
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
+
 app = FastAPI()
+
+database = []
 
 
 @app.get('/', status_code=200, response_model=Message)
@@ -22,3 +27,41 @@ def ola_mundo():
             <h1> Olá Mundo </h1>
         </body>
     </html>"""
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(
+        # Unpacking dict - > Converte os parâmetros para um dict
+        **user.model_dump(),
+        id=len(database) + 1,
+        # username=user.username,
+        # email=user.email,
+        # password=user.password,
+    )
+    database.append(user_with_id)
+    return user_with_id
+
+
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.put(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def update_user(user_id: int, user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Deu ruim! Não achei...'
+        )
+    database[user_id - 1] = user_with_id
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', status_code=HTTPStatus.OK)
+def delete_user(user_id: int):
+    database.pop(user_id - 1)
+    return 'Usuário deletado'
